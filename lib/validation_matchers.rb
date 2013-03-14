836f4d39d3c43810824085ef481a855ed4f1d0b5
Dominik Bamberger <dmayer@suse.de>
2013-03-14 14:38:05 +0100

    remove factory_girl dependency

--- lib/validation_matchers.rb
+++ lib/validation_matchers.rb
@@ -1,4 +1,6 @@
 # Allows for some automagic association tests.
+# Expects 'subject' to be defined. E.g.
+#   subject { FactoryGirl.create :foo }
 
 def must_validate_presence_of(attribute_name)
   it "validates_presence_of :#{attribute_name.to_s.parameterize.underscore}" do
@@ -10,7 +12,7 @@ end
 
 def must_validate_uniqueness_of(attribute_name)
   it "validates_uniqueness_of :#{attribute_name.to_s.parameterize.underscore}" do
-    other = FactoryGirl.create subject.class.to_s.parameterize.underscore.to_sym
+    other = subject.class.new
     subject.send "#{attribute_name}=".to_sym, other.send(attribute_name)
     subject.valid?.must_equal false
     subject.errors.messages[attribute_name.to_sym].must_include "has already been taken"
