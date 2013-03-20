e58b5f260e1344707a9281b2081efd391863f7c8
Dominik Bamberger <dmayer@suse.de>
2013-03-20 16:30:30 +0100

    move all standard validation steps into spec method

--- lib/validation_matchers.rb
+++ lib/validation_matchers.rb
@@ -3,50 +3,39 @@
 #   subject { FactoryGirl.create :foo }
 
 def must_validate_presence_of(attribute, options = {})
-  spec(__method__, attribute, options) do
-    subject.send "#{attribute}=".to_sym, nil
-    subject.wont_be :valid?
-    message = options[:message] || "can't be blank"
-    subject.errors.messages[attribute.to_sym].must_include message
-  end
+  spec(subject, __method__, attribute, options, "can't be blank", nil)
 end
 
 def must_validate_uniqueness_of(attribute, options = {})
-  spec(__method__, attribute, options) do
-    other = subject.class.new
-    other.send "#{attribute}=".to_sym, subject.send(attribute)
-    other.wont_be :valid?
-    message = options[:message] || "has_already been taken"
-    other.errors.messages[attribute.to_sym].must_include message
-  end
+  spec(subject.dup, __method__, attribute, options, "has_already been taken", subject.send(attribute))
 end
 
 def must_validate_length_of(attribute, options = {})
   if options[:minimum]
-    spec(__method__, attribute, options.except(:maximum)) do
-      subject.send "#{attribute}=".to_sym, (0..options[:minimum] - 1).to_a
-      subject.wont_be :valid?
-      message = options[:message] || "is too short (minimum is #{options[:minimum]}"
-      subject.errors.messages[attribute.to_sym].must_include message
-    end
+    spec(
+      subject, __method__, attribute, options.except(:maximum),
+      "is too short (minimum is #{options[:minimum]}", (0..options[:minimum] - 1).to_a
+    )
   end
   if options[:maximum]
-    spec(__method__, attribute, options.except(:minimum)) do
-      subject.send "#{attribute}=".to_sym, (0..options[:maximum] + 1).to_a
-      subject.wont_be :valid?
-      message = options[:message] || "is too long (maximum is #{options[:maximum]}"
-      subject.errors.messages[attribute.to_sym].must_include message
-    end
+    spec(
+      subject, __method__, attribute, options.except(:minimum),
+      "is too long (maximum is #{options[:maximum]}", (0..options[:maximum] + 1).to_a
+    )
   end
 end
 
 
 private
 
-def spec(name, attribute, options)
+def spec(subject, name, attribute, options, default_message, value)
   title = "#{name.to_s[4..-1]} :#{attribute.to_s.parameterize.underscore}"
   title += options.to_s[1..-2].gsub('=>', ' => ') unless options.blank?
   it title do
-    yield
+    yield if block_given?
+    subject.send "#{attribute}=".to_sym, value
+    subject.wont_be :valid?
+    message = options[:message] || default_message
+    subject.errors.messages[attribute.to_sym].must_include message
   end
 end
