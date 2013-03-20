9b43f14ee726711c3a3f43fddace07aa852dee11
Dominik Bamberger <dmayer@suse.de>
2013-03-20 15:40:50 +0100

    add must_validate_length_of

--- lib/validation_matchers.rb
+++ lib/validation_matchers.rb
@@ -20,3 +20,20 @@ def must_validate_uniqueness_of(attribute_name, options = {})
     other.errors.messages[attribute_name.to_sym].must_include message
   end
 end
+
+def must_validate_length_of(attribute_name, options = {})
+  it "validates_length_of :#{attribute_name.to_s.parameterize.underscore}" do
+    if options[:minimum]
+      subject.send "#{attribute_name}=".to_sym, (0..options[:minimum] - 1).to_a
+      subject.wont_be :valid?
+      message = options[:message] || "is too short (minimum is #{options[:minimum]}"
+      subject.errors.messages[attribute_name.to_sym].must_include message
+    end
+    if options[:maximum]
+      subject.send "#{attribute_name}=".to_sym, (0..options[:maximum] + 1).to_a
+      subject.wont_be :valid?
+      message = options[:message] || "is too long (maximum is #{options[:maximum]}"
+      subject.errors.messages[attribute_name.to_sym].must_include message
+    end
+  end
+end
