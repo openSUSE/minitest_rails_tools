10d94711cd3cce92778d2c1e480f39779cb4087f
Dominik Bamberger <dmayer@suse.de>
2013-03-20 15:25:27 +0100

    allow to override validation message

--- lib/validation_matchers.rb
+++ lib/validation_matchers.rb
@@ -2,19 +2,21 @@
 # Expects 'subject' to be defined. E.g.
 #   subject { FactoryGirl.create :foo }
 
-def must_validate_presence_of(attribute_name)
+def must_validate_presence_of(attribute_name, options = {})
   it "validates_presence_of :#{attribute_name.to_s.parameterize.underscore}" do
     subject.send "#{attribute_name}=".to_sym, nil
     subject.valid?.must_equal false
-    subject.errors.messages[attribute_name.to_sym].must_include "can't be blank"
+    message = options[:message] || "can't be blank"
+    subject.errors.messages[attribute_name.to_sym].must_include message
   end
 end
 
-def must_validate_uniqueness_of(attribute_name)
+def must_validate_uniqueness_of(attribute_name, options = {})
   it "validates_uniqueness_of :#{attribute_name.to_s.parameterize.underscore}" do
     other = subject.class.new
     other.send "#{attribute_name}=".to_sym, subject.send(attribute_name)
     other.valid?.must_equal false
-    other.errors.messages[attribute_name.to_sym].must_include "has already been taken"
+    message = options[:message] || "has_already been taken"
+    other.errors.messages[attribute_name.to_sym].must_include message
   end
 end
