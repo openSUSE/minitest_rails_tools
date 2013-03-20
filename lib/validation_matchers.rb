406e6e70113d09ecea46486e3d2050eba02a8669
Dominik Bamberger <dmayer@suse.de>
2013-03-20 16:06:53 +0100

    move spec generation into private function

--- lib/validation_matchers.rb
+++ lib/validation_matchers.rb
@@ -2,38 +2,38 @@
 # Expects 'subject' to be defined. E.g.
 #   subject { FactoryGirl.create :foo }
 
-def must_validate_presence_of(attribute_name, options = {})
-  it title(__method__.to_s[4..-1], attribute_name, options) do
-    subject.send "#{attribute_name}=".to_sym, nil
+def must_validate_presence_of(attribute, options = {})
+  spec(__method__, attribute, options) do
+    subject.send "#{attribute}=".to_sym, nil
     subject.wont_be :valid?
     message = options[:message] || "can't be blank"
-    subject.errors.messages[attribute_name.to_sym].must_include message
+    subject.errors.messages[attribute.to_sym].must_include message
   end
 end
 
-def must_validate_uniqueness_of(attribute_name, options = {})
-  it title(__method__.to_s[4..-1], attribute_name, options) do
+def must_validate_uniqueness_of(attribute, options = {})
+  spec(__method__, attribute, options) do
     other = subject.class.new
-    other.send "#{attribute_name}=".to_sym, subject.send(attribute_name)
+    other.send "#{attribute}=".to_sym, subject.send(attribute)
     other.wont_be :valid?
     message = options[:message] || "has_already been taken"
-    other.errors.messages[attribute_name.to_sym].must_include message
+    other.errors.messages[attribute.to_sym].must_include message
   end
 end
 
-def must_validate_length_of(attribute_name, options = {})
-  it title(__method__.to_s[4..-1], attribute_name, options) do
+def must_validate_length_of(attribute, options = {})
+  spec(__method__, attribute, options) do
     if options[:minimum]
-      subject.send "#{attribute_name}=".to_sym, (0..options[:minimum] - 1).to_a
+      subject.send "#{attribute}=".to_sym, (0..options[:minimum] - 1).to_a
       subject.wont_be :valid?
       message = options[:message] || "is too short (minimum is #{options[:minimum]}"
-      subject.errors.messages[attribute_name.to_sym].must_include message
+      subject.errors.messages[attribute.to_sym].must_include message
     end
     if options[:maximum]
-      subject.send "#{attribute_name}=".to_sym, (0..options[:maximum] + 1).to_a
+      subject.send "#{attribute}=".to_sym, (0..options[:maximum] + 1).to_a
       subject.wont_be :valid?
       message = options[:message] || "is too long (maximum is #{options[:maximum]}"
-      subject.errors.messages[attribute_name.to_sym].must_include message
+      subject.errors.messages[attribute.to_sym].must_include message
     end
   end
 end
@@ -41,8 +41,10 @@ end
 
 private
 
-def title(name, attribute_name, options)
-  title = "#{name} :#{attribute_name.to_s.parameterize.underscore}"
+def spec(name, attribute, options)
+  title = "#{name.to_s[4..-1]} :#{attribute.to_s.parameterize.underscore}"
   title += options.to_s[1..-2].gsub('=>', ' => ') unless options.blank?
-  title
+  it title do
+    yield
+  end
 end
