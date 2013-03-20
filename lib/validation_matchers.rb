7c995b6413a20afc4bb42f4faefffb887fea8012
Dominik Bamberger <dmayer@suse.de>
2013-03-20 15:26:41 +0100

    use correct minitest syntax

--- lib/validation_matchers.rb
+++ lib/validation_matchers.rb
@@ -5,7 +5,7 @@
 def must_validate_presence_of(attribute_name, options = {})
   it "validates_presence_of :#{attribute_name.to_s.parameterize.underscore}" do
     subject.send "#{attribute_name}=".to_sym, nil
-    subject.valid?.must_equal false
+    subject.wont_be :valid?
     message = options[:message] || "can't be blank"
     subject.errors.messages[attribute_name.to_sym].must_include message
   end
@@ -15,7 +15,7 @@ def must_validate_uniqueness_of(attribute_name, options = {})
   it "validates_uniqueness_of :#{attribute_name.to_s.parameterize.underscore}" do
     other = subject.class.new
     other.send "#{attribute_name}=".to_sym, subject.send(attribute_name)
-    other.valid?.must_equal false
+    other.wont_be :valid?
     message = options[:message] || "has_already been taken"
     other.errors.messages[attribute_name.to_sym].must_include message
   end
