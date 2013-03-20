864f69fa4000cd32f39ffaad88676d38705d3b2f
Dominik Bamberger <dmayer@suse.de>
2013-03-20 15:47:49 +0100

    refactor title generation to include options hash

--- lib/validation_matchers.rb
+++ lib/validation_matchers.rb
@@ -3,7 +3,7 @@
 #   subject { FactoryGirl.create :foo }
 
 def must_validate_presence_of(attribute_name, options = {})
-  it "validates_presence_of :#{attribute_name.to_s.parameterize.underscore}" do
+  it title('validates_presence_of', attribute_name, options) do
     subject.send "#{attribute_name}=".to_sym, nil
     subject.wont_be :valid?
     message = options[:message] || "can't be blank"
@@ -12,7 +12,7 @@ def must_validate_presence_of(attribute_name, options = {})
 end
 
 def must_validate_uniqueness_of(attribute_name, options = {})
-  it "validates_uniqueness_of :#{attribute_name.to_s.parameterize.underscore}" do
+  it title('validates_uniqueness_of', attribute_name, options) do
     other = subject.class.new
     other.send "#{attribute_name}=".to_sym, subject.send(attribute_name)
     other.wont_be :valid?
@@ -22,7 +22,7 @@ def must_validate_uniqueness_of(attribute_name, options = {})
 end
 
 def must_validate_length_of(attribute_name, options = {})
-  it "validates_length_of :#{attribute_name.to_s.parameterize.underscore}" do
+  it title('validates_length_of', attribute_name, options) do
     if options[:minimum]
       subject.send "#{attribute_name}=".to_sym, (0..options[:minimum] - 1).to_a
       subject.wont_be :valid?
@@ -36,4 +36,13 @@ def must_validate_length_of(attribute_name, options = {})
       subject.errors.messages[attribute_name.to_sym].must_include message
     end
   end
+end
+
+
+private
+
+def title(name, attribute_name, options)
+  title = "#{name} :#{attribute_name.to_s.parameterize.underscore}"
+  title += options.to_s[1..-2].gsub('=>', ' => ') unless options.blank?
+  title
 end
