5c3ab6ae7194123040e3ecb928dbcbc41562e1af
Dominik Bamberger <dmayer@suse.de>
2013-03-21 16:29:25 +0100

    Version 0.1.8 - move controller setup to separate class

--- lib/application_integration.rb
+++ lib/application_integration.rb
@@ -1,12 +1,17 @@
 class MiniTest::Spec
+  include Rails.application.routes.url_helpers
+end
+
+class ControllerSpec < MiniTest::Spec
 
   require 'rails/test_help'
 
   alias :method_name :__name__ if defined? :__name__
 
-  include Rails.application.routes.url_helpers
   include ActiveSupport::Testing::SetupAndTeardown
   include ActiveSupport::Testing::Assertions
   include ActionController::TestCase::Behavior
 
 end
+# Functional tests = describe ***Controller
+MiniTest::Spec.register_spec_type( /Controller$/, ControllerSpec )
